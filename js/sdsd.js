fb2780154871b45e674b6dd26d5fe1011295ab63
Sergio Sainz <sergiosainz.proyectos@gmail.com>
2022-07-09 18:51:57 -0300

    arreglos .js

---
+++ js/sdsd.js
@@ -0,0 +1,850 @@
+
+
+
+
+
+//               F O R M A S
+
+// CÍRCULO
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// CUADRADO
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// RECTÁNGULO
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+//TRIÁNGULO
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+//               L E T R A S
+
+// A
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// B
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// C
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// D
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// E
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// F
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// G
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// H
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// I
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// J
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// K
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// L
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// M
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// Ñ
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// O
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// P
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// Q
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// R
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// S
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// T
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// U
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// V
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// W
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// X
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// Y
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// Z
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+
+//               N Ú M E R O S
+
+// 0
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// 1
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// 2
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// 3
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// 4
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// 5
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// 6
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// 7
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// 8
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
+
+
+// 9
+
+var fotopordefecto1 = 'leon1';
+
+function cambiafoto1()
+{   
+    var imagen1 = document.getElementById('leon1');
+    if (fotopordefecto1 == 'leon1'){
+        imagen1.src = './img/animales/leon2.jpg.';
+        fotopordefecto1 = 'leon2';
+    }
+    else {
+        imagen1.src = './img/animales/leon1.jpg';
+        fotopordefecto1 = 'leon1';
+    }
+}
+
+const audio1 = new Audio();
+audio1.src = './mp3/animales/rugido.mp3';
